717cddd17ffdc50e2cdc4fe024d2343b7b2212cf
Felipe Rocha <rocha.felipe98@gmail.com>
2024-09-09 13:46:48 -0300

    Adicionar associação com Invoice

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  has_many :invoices
+
+  enum role: {user: 0, admin: 1 } #Definindo a roles
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters" }
+  validates :email, length: { maximum: 255 }
+  validates :password, length: {maximum: 8 }
+
   # Include default devise modules. Others available are:
   #  :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
@@ -6,11 +14,5 @@ class User < ApplicationRecord
          :recoverable, 
          :rememberable, 
          :validatable,
-         :confirmable,
-  
-  enum role: {user: 0, admin: 1 } #Definindo a roles
-  
-  validates :name, presence: true, length: { minimum: 2, maximum: 50 }, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters" }
-  validates :email, length: { maximum: 255 }
-  validates :password, length: {maximum: 8 }
+         :confirmable, 
 end
