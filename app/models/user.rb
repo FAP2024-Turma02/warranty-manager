deae8cd3a2e9c38d28795fecde418ed413940abf
Felipe Rocha <rocha.felipe98@gmail.com>
2024-09-08 22:03:48 -0300

    Configurar roles e validar Name, Email e Password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  #  :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, 
+         :registerable,
+         :recoverable, 
+         :rememberable, 
+         :validatable,
+         :confirmable,
+  
+  enum role: {user: 0, admin: 1 } #Definindo a roles
+  
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters" }
+  validates :email, length: { maximum: 255 }
+  validates :password, length: {maximum: 8 }
 end
--- db/migrate/20240907152319_create_users.rb
+++ db/migrate/20240907152319_create_users.rb
@@ -1,11 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password
-      t.enum :role
-      t.boolean :admin
+      t.string :name      
+      t.enum :role, default: 0, null: false
+      t.boolean :admin,  default: 0, 
 
       t.timestamps
     end
