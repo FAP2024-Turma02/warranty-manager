421e86bcac7ea614e61ff7c7f48d28dc2b81746a
dani2fraga <dani2fraga@yahoo.com.br>
2024-09-17 10:51:58 -0300

    cria model store

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_13_123731) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_16_202007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name", limit: 45
+    t.string "contact", limit: 45
+    t.string "address", limit: 45
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
