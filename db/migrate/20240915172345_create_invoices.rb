7c6250320e8b294c03dcbbeb76e8b857e14543e2
FabioBezerradeAndrade <75230523+FabioBezerradeAndrade@users.noreply.github.com>
2024-09-17 16:07:50 -0300

    remove relacionamento entre as tabelas invoice e user

--- db/migrate/20240915172345_create_invoices.rb
+++ db/migrate/20240915172345_create_invoices.rb
@@ -2,10 +2,8 @@ class CreateInvoices < ActiveRecord::Migration[7.0]
   def change
     create_table :invoices do |t|
       t.string :invoice_number, null: false
-      t.binary :pdf  
       t.date :purchase_date, null: false
       t.date :issue_date, null: false
-      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -44,13 +44,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_15_173156) do
 
   create_table "invoices", force: :cascade do |t|
     t.string "invoice_number", null: false
-    t.binary "pdf"
     t.date "purchase_date", null: false
     t.date "issue_date", null: false
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_invoices_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -74,5 +71,4 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_15_173156) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "invoices", "users"
 end
