ba5bcab32e0a7de870612757dd64111a3af870fa
NoteFelipe <rocha.felipe98@gmail.com>
2024-09-21 16:49:14 -0300

    Configurar Devise_token_auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :warranties, only: [:index, :show]
+  #devise_for :users
+  mount_devise_token_auth_for 'User', at: 'auth'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "home#index"
+  #
+  # get 'users', to 1user#index'
+  # resources :companies, except: [:destroy, :updatr]
+
 end
