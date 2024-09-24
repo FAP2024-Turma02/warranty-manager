eeac886a67ed0999b01f0970b9accf3604bad4de
RenanAmendola <102184927+RenanAmendola@users.noreply.github.com>
2024-09-24 09:40:35 -0300

    Clean code feito
    Um leve ajuste no codigo para manter o padrão

--- app/models/warranty.rb
+++ app/models/warranty.rb
@@ -1,7 +1,5 @@
 class Warranty < ApplicationRecord
+    validates :warranty_number, :issue_date, :expirity_date, :validity_period, presence: true
 
     belongs_to -> :product
-  
-    validates :warranty_number, :issue_date, :expirity_date, :validity_period,
-               presence: true
   end
