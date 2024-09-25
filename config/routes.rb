ad0aebfb7afb205e651d394411299f7200171e51
Felipe Rocha <rocha.felipe98@gmail.com>
2024-09-25 16:36:18 -0300

    Configurar o user_controller e routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   #devise_for :users
   mount_devise_token_auth_for 'User', at: 'auth'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "home#index"
-  #
-  # get 'users', to 1user#index'
-  # resources :companies, except: [:destroy, :updatr]
+  # Usando resources para User, por exemplo
+  resources :users, only: [:index, :show, :update, :destroy]
 
 end
