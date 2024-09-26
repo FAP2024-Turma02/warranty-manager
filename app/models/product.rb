9b68d681313d3b3700bd7a2f673e6d9807f85794
RenanAmendola <102184927+RenanAmendola@users.noreply.github.com>
2024-09-25 23:42:32 -0300

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
     validates :name, presence: true, length: { maximum: 45 }
     validates_presence_of :price, :serial_number, :warranty_expiry_date, :invoice_id, :store_id
 
-    has_many -> :warranty
+    has_many :warranty
 end
