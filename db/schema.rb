653419e862a66513e241657061ec617303700df6
Carlosguimara <carlos.henrique.ccruz@gmail.com>
2024-09-27 11:44:13 -0300

    Compatibilzar algumas restrições  pendentes entre o model store  e o  BD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_27_135617) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_27_143803) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,9 +64,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_27_135617) do
   end
 
   create_table "stores", force: :cascade do |t|
-    t.string "name", limit: 45
-    t.string "contact", limit: 45
-    t.string "address", limit: 45
+    t.string "name", limit: 45, null: false
+    t.string "contact", limit: 45, null: false
+    t.string "address", limit: 45, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
