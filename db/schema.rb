023876b3f3c0249cd17be73ae5491f97480fc875
Felipe Rocha <rocha.felipe98@gmail.com>
2024-09-26 21:54:48 -0300

    Configurar a rota auth/signin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_25_181258) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_27_004041) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,14 +37,20 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_25_181258) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name", null: false
     t.integer "role", default: 0
     t.boolean "admin", default: false
-    t.string "provider"
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.json "tokens"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   create_table "warranties", force: :cascade do |t|
