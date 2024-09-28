bcc485f2997687c2754572371d9e06eae6f9b6a7
Felipe Rocha <rocha.felipe98@gmail.com>
2024-09-28 10:17:21 -0300

    Adicionar no User_controller Index e Show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,21 +11,6 @@ class UsersController < ApplicationController
     render json: @user
   end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: { error: 'Failed to update user' }, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    render json: { message: 'User deleted successfully' }
-  end
-
   private
 
   def user_params
