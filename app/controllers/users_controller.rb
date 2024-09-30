f4ee22d2973265af115cc48a4009033428e4e83f
Felipe Rocha <rocha.felipe98@gmail.com>
2024-09-30 16:49:28 -0300

    Corrigindo PR#14

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!, only: [:index, :show, :update, :destroy]
+    before_action :authenticate_user!, except: :create
 
   def index
     @users = User.all
