bda5122afaab2798c39f86e28c7148fc94bdae18
Felipe Rocha <rocha.felipe98@gmail.com>
2024-09-30 16:49:28 -0300

    Corrigindo PR#14

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-
   resources :users, except: :create
   resources :invoices, only: [:index, :show]
   resources :warranties, only: [:index, :show]
