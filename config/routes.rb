a9b9fbb197bf4433f1b8f9636a1a38bcabfde0c0
Felipe Rocha <rocha.felipe98@gmail.com>
2024-09-30 16:49:28 -0300

    Corrigindo PR#14

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   mount_devise_token_auth_for 'User', at: 'auth'
 
-  # Usando resources para User, por exemplo
-  resources :users, only: [:index, :show, :update, :destroy]
+  resources :users, except: :create
 
 end
