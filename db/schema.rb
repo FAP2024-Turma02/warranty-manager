8efe641ef4bc0d894b0012e93a0a18d7a7a2b882
FabioBezerradeAndrade <75230523+FabioBezerradeAndrade@users.noreply.github.com>
2024-09-30 16:00:29 -0300

    add schema.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2024_09_23_144611) do
-
+ActiveRecord::Schema[7.0].define(version: 2024_09_27_172702) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_23_144611) do
     t.date "issue_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_invoices_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -100,11 +100,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_23_144611) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "products_id", null: false
+    t.bigint "product_id", null: false
+    t.index ["product_id"], name: "index_warranties_on_product_id"
     t.index ["products_id"], name: "index_warranties_on_products_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "invoices", "users"
+  add_foreign_key "warranties", "products"
   add_foreign_key "warranties", "products", column: "products_id"
-  
 end
