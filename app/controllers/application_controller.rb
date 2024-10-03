d089e04586dc5dc359429db240be2dbcf113509c
Mathias <mathiasernandes02@hotmail.com>
2024-10-03 16:13:37 -0300

    corrigindo erro de duplicação

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-  include DeviseTokenAuth::Concerns::SetUserByToken
   include Pundit::Authorization
 end
