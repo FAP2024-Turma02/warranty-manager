2d7302e427bcd7ac6b0753291f68b981543d3aad
Mathias <mathiasernandes02@hotmail.com>
2024-10-03 13:57:00 -0300

    Acrescentando a gem pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  include Pundit::Authorization
 end
