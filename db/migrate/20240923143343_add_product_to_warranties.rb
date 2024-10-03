1f5a683c8ee569a7211d8205f7ce4b99a725679c
RenanAmendola <102184927+RenanAmendola@users.noreply.github.com>
2024-10-03 09:24:26 -0300

    Update 20240923143343_add_product_to_warranties.rb

--- db/migrate/20240923143343_add_product_to_warranties.rb
+++ db/migrate/20240923143343_add_product_to_warranties.rb
@@ -1,5 +1,5 @@
 class AddProductToWarranties < ActiveRecord::Migration[7.0]
   def change
-    add_reference :warranties, :products, null: false, foreign_key: true
+    add_reference :warranties, :product, null: false, foreign_key: true
   end
 end
