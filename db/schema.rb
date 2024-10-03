4d8f19dc4e21e7ed630e7f68c7ed0f775a6ceab5
Felipe Rocha <rocha.felipe98@gmail.com>
2024-10-03 11:30:59 -0300

    Fix/redundancia_schema

--- db/schema.rb
+++ db/schema.rb
@@ -109,6 +109,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_01_004909) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "warranties", "products"
   add_foreign_key "warranties", "products", column: "products_id"
 end
