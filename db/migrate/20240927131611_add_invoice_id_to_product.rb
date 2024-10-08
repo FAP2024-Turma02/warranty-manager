aeb8603baf0dafd6fb5504b3dfd1c8bfd5e0f6ba
RenanAmendola <102184927+RenanAmendola@users.noreply.github.com>
2024-10-08 09:25:16 -0300

    Update 20240927131611_add_invoice_id_to_product.rb

--- db/migrate/20240927131611_add_invoice_id_to_product.rb
+++ db/migrate/20240927131611_add_invoice_id_to_product.rb
@@ -1,5 +1,5 @@
 class AddInvoiceIdToProduct < ActiveRecord::Migration[7.0]
   def change
-    add_reference :products, :invoices, column: :invoice_id
+    add_reference :products, :invoices, foreign_key: true, index: true
   end
 end
