edf89a0c0bf71a4796daf724a61ce39631d589ff
Felipe Rocha <rocha.felipe98@gmail.com>
2024-10-09 09:43:24 -0300

    add lockable to user

--- db/migrate/20241009120927_devise_token_auth_create_users.rb
+++ db/migrate/20241009120927_devise_token_auth_create_users.rb
@@ -9,12 +9,18 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[7.0]
       ## Recoverable
       t.boolean  :allow_password_change, :default => false
 
+      ## Lockable
+      t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      t.string   :unlock_token # Only if unlock strategy is :email or :both
+      t.datetime :locked_at
+
       ## Tokens
       t.json :tokens
     end
 
     add_index :users, [:uid, :provider],     unique: true
     add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :users, :unlock_token,         unique: true
+    
   end
 end
