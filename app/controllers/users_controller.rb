88db2eddd4ea67d89474935bdd6c729a7389985e
Felipe Rocha <rocha.felipe98@gmail.com>
2024-10-18 10:33:13 -0300

    Adicionar create e update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def index
     @users = User.all
     render json: @users
@@ -9,11 +8,33 @@ class UsersController < ApplicationController
     render json: user
   end
 
-  private 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: { status: 'success', message: 'Usuário criado com sucesso', data: @user }, status: :created
+    else
+      render json: { status: 'error', message: 'Falha ao criar usuário', errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
 
   def user
     @user ||= User.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     render json: { error: "User not found" }, status: :not_found
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :role)
   end
 end
