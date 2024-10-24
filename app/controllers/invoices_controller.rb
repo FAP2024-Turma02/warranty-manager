68c1fecd5bcd41389ca22c705fb2d1d3c1e8f36d
FabioBezerradeAndrade <75230523+FabioBezerradeAndrade@users.noreply.github.com>
2024-10-24 04:30:07 -0300

    cria rota e controller create e update

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,20 +1,46 @@
 class InvoicesController < ApplicationController
-    before_action :set_invoice, only: :show
-  
-    def index
-      @invoices = Invoice.all
-      render json: @invoices
+  before_action :authenticate_user!
+  before_action :set_invoice, only: [:show, :update]
+  before_action :authorize_invoice, only: [:create, :update]
+
+  def index
+    @invoices = Invoice.all
+    render json: @invoices.map { |invoice| InvoiceSerializer.call(invoice) }
+  end
+
+  def show
+    render json: InvoiceSerializer.call(@invoice)
+  end
+
+  def create
+    @invoice = Invoice.new(invoice_params)
+
+    if @invoice.save
+      render json: InvoiceSerializer.call(@invoice), status: :created
+    else
+      render json: { errors: @invoice.errors.full_messages }, status: :unprocessable_entity
     end
-  
-    def show
-      render json: @invoice
+  end
+
+  def update
+    if @invoice.update(invoice_params)
+      render json: InvoiceSerializer.call(@invoice), status: :ok
+    else
+      render json: { errors: @invoice.errors.full_messages }, status: :unprocessable_entity
     end
-  
-    private
-  
-    def set_invoice
-      @invoice = Invoice.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: "Invoice not found" }, status: :not_found
-    end
+  end
+
+  private
+
+  def set_invoice
+    @invoice ||= Invoice.find(params[:id])
+  end
+
+  def invoice_params
+    params.require(:invoice).permit(:invoice_number, :purchase_date, :issue_date, :pdf, :user_id)
+  end
+
+  def authorize_invoice
+    authorize @invoice || Invoice
+  end
 end
