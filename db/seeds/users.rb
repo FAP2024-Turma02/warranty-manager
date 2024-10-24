26c875125e3b2804e7c776c2735b4ed17e39447b
FabioBezerradeAndrade <75230523+FabioBezerradeAndrade@users.noreply.github.com>
2024-10-24 06:26:23 -0300

    ajusta seed user para uid tokenAuth

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -6,17 +6,29 @@ users_data = [
   { name: 'Josefa Barbosa', email: 'zefinha@example.com', password: 'password', role: 'user', admin: 0 },
   { name: 'Frederico Evandro', email: 'vela.libra@example.com', password: 'password', role: 'admin', admin: 1 },
   { name: 'Sivirino Valentin', email: 'biu@example.com', password: 'password', role: 'user', admin: 0 },
-  { name: 'Raimunda Ferreira', email: 'mundica@example.com', password: 'password', role: 'user', admin: 0 },
-  { name: 'Manuel da Silva', email: 'maned@example.com', password: 'password', role: 'user', admin: 0 },
+  { name: 'Raimunda Ferreira', email: 'mundica@example.com', password: 'password', role: 'user', admin: 1 },
+  { name: 'Manuel da Silva', email: 'maned@example.com', password: 'password', role: 'admin', admin: 0 },
   { name: 'Eduardo Costa', email: 'du.dudu.edu@example.com', password: 'password', role: 'admin', admin: 1 }
 ]
 
 users_data.each do |user_data|
   unless User.exists?(email: user_data[:email])
-    User.create!(user_data)
-    puts I18n.t('seed.users.success', email: user_data[:email])
+    user = User.new(
+      email: user_data[:email],
+      name: user_data[:name],
+      password: user_data[:password],
+      uid: user_data[:email], 
+      provider: 'email',
+      role: user_data[:role],
+      admin: user_data[:admin]  
+    )
+
+    if user.save
+      puts I18n.t('seed.users.success', email: user.email)
+    else
+      puts I18n.t('seed.users.error', message: user.errors.full_messages.join(', '))
+    end
   else
     puts I18n.t('seed.users.already_exists', email: user_data[:email])
   end
 end
-
