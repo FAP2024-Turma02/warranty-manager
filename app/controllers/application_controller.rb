180a1211376426abd3a8c2fdde3cc6c74af6c678
Felipe Rocha <rocha.felipe98@gmail.com>
2024-10-30 09:01:37 -0300

    Ajustando o aplication controller para aceitar parametros especificos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Pundit::Authorization
 
@@ -10,5 +12,11 @@ class ApplicationController < ActionController::API
     model_name = exception.model.constantize.model_name.human
     render json: { error: I18n.t('errors.not_found', model: model_name) }, status: :not_found
   end
-  
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :name])
+  end
 end
