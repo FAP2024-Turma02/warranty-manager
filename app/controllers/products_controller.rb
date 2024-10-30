1dcf50c004f30553b0eaa6f74267b2300d68dc67
Renan A <renanamendola@hotmail.com>
2024-10-30 13:58:12 -0300

    Adicionado Create e update ao product crontroller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,42 @@
 class ProductsController < ApplicationController
     
     def index
-        @products = Product.all
-        render json: @products
+        @product = Product.all
+        render json: @product
     end
 
     def show
         render json: product
     end
 
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+          render json: { status: 'success', message: 'Produto criado com sucesso', data: @product }, status: :created
+        else
+          render json: { status: 'error', message: 'Falha ao criar produto', errors: @product.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+    
+    def update
+        @product = Product.find(params[:id])
+        if @product.update(product_params)
+          redirect_to @product
+        else
+          render :edit, status: :unprocessable_entity
+    end
+
+
     private
 
     def product 
-        @products ||= Product.find(params[:id])
+        @product ||= Product.find(params[:id])
     rescue ActiveRecord::RecordNotFound
         render json: { error: 'Product not found' }, status: :not_found
+    end
+
+    def product_params
+        params.require(:product).permit(:name, :price, :serial_number, :warranty_expiry_date, :invoice_id, :store_id)
     end
 
 end
