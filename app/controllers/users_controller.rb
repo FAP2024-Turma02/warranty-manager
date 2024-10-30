2d1edfb96440cb788505b16594fa8f4ce07565bf
Felipe Rocha <rocha.felipe98@gmail.com>
2024-10-30 08:37:23 -0300

    Ajustando o controller user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,25 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    render json: @user
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: { status: 'success', message: 'Usuário criado com sucesso', data: @user }, status: :created
-    else
-      render json: { status: 'error', message: 'Falha ao criar usuário', errors: @user.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to @user
-    else
-      render :edit, status: :unprocessable_entity
-    end
+    render json: user
   end
 
   private
