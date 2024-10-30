efc4ad8636dd65978fae7330c378e5cc74042d4b
Felipe Rocha <rocha.felipe98@gmail.com>
2024-10-30 08:37:23 -0300

    Ajustando o controller user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   resources :invoices, only: [:index, :show]
   resources :warranties, only: [:index, :show]
-  resources :users, only: [:index, :show, :create, :update]
+  resources :users, only: [:index, :show]
   resources :stores, only: [:index, :show]
 end
