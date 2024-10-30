086889cc5bfee450e5a512313eff9dfda88b1697
Felipe Rocha <rocha.felipe98@gmail.com>
2024-10-30 09:01:37 -0300

    Ajustando o aplication controller para aceitar parametros especificos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Pundit::Authorization
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :name])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# Seed para o usuário administrador
+User.create!(
+  name: "Admin User",
+  email: "admin@example.com",
+  password: "password123",
+  password_confirmation: "password123",
+  confirmed_at: Time.parse("2024-10-28T12:00:00Z"),
+  role: 1,         # Assumindo que o papel 1 representa o admin
+  admin: true,     # Define como administrador
+  uid: "admin@example.com",
+  provider: "email"
+)
+
+# Seed para um usuário normal
+User.create!(
+  name: "Regular User",
+  email: "user@example.com",
+  password: "password123",
+  password_confirmation: "password123",
+  confirmed_at: Time.current,
+  role: 0,         # Assumindo que o papel 0 representa um usuário normal
+  admin: false,    # Não é administrador
+  uid: "user@example.com",
+  provider: "email"
+)
