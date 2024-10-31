9d9d963e46e80c9c47e1face03375e30a3965b74
FabioBezerradeAndrade <75230523+FabioBezerradeAndrade@users.noreply.github.com>
2024-10-30 22:05:46 -0300

    cria politicas da invoice em policies

--- app/policies/invoice_policy.rb
+++ app/policies/invoice_policy.rb
@@ -1,10 +1,31 @@
 class InvoicePolicy < ApplicationPolicy
-    def create?
-      user.admin? || user.id == record.user_id
-    end
-  
-    def update?
-      user.admin? || user.id == record.user_id
+  class Scope < Scope
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(user_id: user.id)
+      end
     end
   end
-  
+
+  def index?
+    user.admin?
+  end
+
+  def show?
+    user.admin? || record.user_id == user.id
+  end
+
+  def create?
+    user.present?
+  end
+
+  def update?
+    if user.admin?
+      true
+    else
+      user.role == 'user' && record.user_id == user.id
+    end
+  end
+end
