b798430eff79d53952ae18b94a0e567e6ffc70d5
Renan A <renanamendola@hotmail.com>
2024-10-31 13:08:52 -0300

    Lançando alterações pedidas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
-  include DeviseTokenAuth::Concerns::SetUserByToken
+  #include DeviseTokenAuth::Concerns::SetUserByToken
   include Pundit::Authorization
+
+  rescue_from ActiveRecord::RecordNotFound, with: record_not_found
+
+  private 
+
+  def record_not_found
+    render json: { error: 'Product not found'}, status: :not_found
+  end
 end
