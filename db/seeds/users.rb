cbadedcba905d2e81eca5294c4be4d283f8305a4
FabioBezerradeAndrade <75230523+FabioBezerradeAndrade@users.noreply.github.com>
2024-10-30 22:10:09 -0300

    defini os usuarios criados como confirmados

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -20,7 +20,8 @@ users_data.each do |user_data|
       uid: user_data[:email], 
       provider: 'email',
       role: user_data[:role],
-      admin: user_data[:admin]  
+      admin: user_data[:admin],
+      confirmed_at: Time.current  
     )
 
     if user.save
