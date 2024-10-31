159d5a9adeb2d244ef7b5a60c757d38643e4fc72
FabioBezerradeAndrade <75230523+FabioBezerradeAndrade@users.noreply.github.com>
2024-10-30 22:04:39 -0300

    ativa o armazenamento de sessao baseado em cookies

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module WarrantyManager
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_warranty_manager_session'
+
   end
 end
