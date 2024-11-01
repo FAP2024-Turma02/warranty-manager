664b102f6c16b832c2db14ad4c73046290806c58
Renan A <renanamendola@hotmail.com>
2024-11-01 10:56:20 -0300

    Ajustado para que fique invoice_id

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
     validates :name, presence: true, length: { maximum: 45 }
-    validates_presence_of :price, :serial_number, :warranty_expiry_date, :invoices_id, :store_id
+    validates_presence_of :price, :serial_number, :warranty_expiry_date, :invoice_id, :store_id
 
     has_many :warranty
     belongs_to :store
