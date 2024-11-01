886b8698aa8ab43b999944a48812fbade4cec0ce
FabioBezerradeAndrade <75230523+FabioBezerradeAndrade@users.noreply.github.com>
2024-11-01 08:31:42 -0300

    corrigi o  controller invoice e application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,12 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Pundit::Authorization
 
+  before_action :authenticate_user!, unless: :devise_controller?
+
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   private
 
@@ -15,11 +18,15 @@ class ApplicationController < ActionController::API
     model_name = exception.model.constantize.model_name.human
     render json: { error: I18n.t('errors.not_found', model: model_name) }, status: :not_found
   end
-  
+
   def user_not_authorized(exception)
     policy_name = exception.policy.class.to_s.underscore
     render json: { error: I18n.t("errors.#{policy_name}.#{exception.query}") },
            status: :forbidden
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
   end
 
   protected
--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,43 +1,30 @@
 class InvoicesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_invoice, only: [:show, :update]
-  after_action :verify_authorized, except: :index
-  
+
   def index
     @invoices = policy_scope(Invoice)
     render json: @invoices.map { |invoice| InvoiceSerializer.call(invoice) }
   end
 
   def show
+    @invoice = Invoice.find(params[:id])
     authorize @invoice
     render json: InvoiceSerializer.call(@invoice)
   end
 
   def create
-    @invoice = current_user.invoices.new(invoice_params)
-    authorize @invoice
-
-    if @invoice.save
-      render json: InvoiceSerializer.call(@invoice), status: :created
-    else
-      render json: { errors: @invoice.errors.full_messages }, status: :unprocessable_entity
-    end
+    @invoice = current_user.invoices.create(invoice_params)
+    authorize Invoice
+    render json: InvoiceSerializer.call(@invoice), status: :created
   end
 
   def update
+    @invoice = Invoice.find(params[:id])
     authorize @invoice
-    if @invoice.update(invoice_params)
-      render json: InvoiceSerializer.call(@invoice), status: :ok
-    else
-      render json: { errors: @invoice.errors.full_messages }, status: :unprocessable_entity
-    end
+    @invoice.update!(invoice_params)
+    render json: InvoiceSerializer.call(@invoice), status: :ok
   end
 
   private
-
-  def set_invoice
-    @invoice ||= Invoice.find(params[:id])
-  end
 
   def invoice_params
     params.require(:invoice).permit(:invoice_number, :purchase_date, :issue_date, :pdf)
