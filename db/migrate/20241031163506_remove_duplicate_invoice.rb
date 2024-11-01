283e43e5ab8c960277ad78ddb7f113217bfef026
Renan A <renanamendola@hotmail.com>
2024-11-01 10:48:08 -0300

    Ajuste de schema feito para ficar apenas o invoice_id

--- db/migrate/20241031163506_remove_duplicate_invoice.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDuplicateInvoice < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :products, :invoice_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_31_163506) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_01_134039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,8 +62,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_31_163506) do
     t.integer "store_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "invoices_id"
-    t.index ["invoices_id"], name: "index_products_on_invoices_id"
+    t.bigint "invoice_id", null: false
+    t.index ["invoice_id"], name: "index_products_on_invoice_id"
   end
 
   create_table "stores", force: :cascade do |t|
@@ -117,6 +117,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_31_163506) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "invoices", "users"
-  add_foreign_key "products", "invoices", column: "invoices_id"
+  add_foreign_key "products", "invoices"
   add_foreign_key "warranties", "products"
 end
