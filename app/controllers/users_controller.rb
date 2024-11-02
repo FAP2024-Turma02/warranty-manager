9dd3d2166465c7e03e22cfcdb5083de77c865950
Felipe Rocha <rocha.felipe98@gmail.com>
2024-11-02 15:25:23 -0300

    Configurando o policy do user, modificando serializer e adicionando a exeption de user_policy.index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,23 +2,35 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    authorize User
     @users = policy_scope(User)
+
     render json: @users.map { |user| UserSerializer.call(user) }
   end
 
   def show
-    render json: UserSerializer.call(@user)
+    authorize user
+
+    render json: UserSerializer.call(user)
+  end
+
+  def create
+    authorize user
+    @user = User.create!(permitted_attributes(User))
+
+    render json: UserSerializer.call(user), status: :created
+  end
+
+  def update
+    authorize user
+    user.update!(permitted_attributes(User))
+
+    render json: UserSerializer.call(user), status: :ok
   end
 
   private
 
   def user
     @user ||= User.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    render json: { error: "User not found" }, status: :not_found
-  end
-
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name, :role)
   end
 end
