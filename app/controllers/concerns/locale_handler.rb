139023bb99ac2a233f27b05ddda4b57d08823876
FabioBezerradeAndrade <75230523+FabioBezerradeAndrade@users.noreply.github.com>
2024-11-13 01:10:36 -0300

    cria arquivos handler de tratamento de mensagens

---
+++ app/controllers/concerns/locale_handler.rb
@@ -0,0 +1,23 @@
+module LocaleHandler
+    extend ActiveSupport::Concern
+  
+    included do
+      before_action :set_locale
+    end
+  
+    private
+  
+    def extract_locale_from_accept_language_header
+      if request.env['HTTP_ACCEPT_LANGUAGE']
+        language = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}(?:-[A-Z]{2})?/).first
+        return language if I18n.available_locales.include?(language&.to_sym)
+      end
+      nil
+    end
+  
+    def set_locale
+      locale = extract_locale_from_accept_language_header || I18n.default_locale
+      Rails.logger.info "Locale set to #{locale}"
+      I18n.locale = locale
+    end
+  end
