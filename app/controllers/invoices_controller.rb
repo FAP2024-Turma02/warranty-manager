8837d5d028f627676022f00f657218459775ec18
FabioBezerradeAndrade <75230523+FabioBezerradeAndrade@users.noreply.github.com>
2024-11-13 01:07:42 -0300

    define o metodo invoice pra fazer a busca do objeto

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,36 +1,42 @@
 class InvoicesController < ApplicationController
   def index
     @invoices = policy_scope(Invoice)
+
     render json: @invoices.map { |invoice| InvoiceSerializer.call(invoice) }
   end
 
   def show
-    @invoice = Invoice.find(params[:id])
-    authorize @invoice
+    authorize invoice
+
     render json: InvoiceSerializer.call(@invoice)
   end
 
   def create
+    authorize Invoice
     @invoice = current_user.invoices.create(invoice_params)
-    authorize @invoice
+
     render json: InvoiceSerializer.call(@invoice), status: :created
   end
 
   def update
-    @invoice = Invoice.find(params[:id])
-    authorize @invoice
+    authorize invoice
     @invoice.update!(invoice_params)
+
     render json: InvoiceSerializer.call(@invoice), status: :ok
   end
 
   def destroy
-    @invoice = Invoice.find(params[:id])
-    authorize @invoice
-    @invoice.destroy
+    authorize invoice
+    invoice.destroy
+
     render_deletion_message('Invoice')
   end
 
   private
+
+  def invoice
+    @invoice = Invoice.find(params[:id])
+  end
 
   def invoice_params
     params.require(:invoice).permit(:invoice_number, :purchase_date, :issue_date, :pdf)
