ea34b26f5139cb8542a2629ffb216d7fa004d0ca
MathiasSoares434 <mathiasernandes02@hotmail.com>
2024-11-14 11:05:11 -0300

    mudando a user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
  include DeviseTokenAuth::Concerns::User
  has_many :invoices
- 
+ has_many :product, through: :invoice
  devise :database_authenticatable, :registerable, :recoverable,
         :rememberable, :validatable, :lockable
-  
- enum role: {user: 0, admin: 1}  
 
- validates :name, presence: true, length: { minimum: 3, maximum: 50 }, format: { with: /\A[a-zA-Z\s]+\z/, message: "Utilize somente letras" } 
+ enum role: {user: 0, admin: 1}
+
+ validates :name, presence: true, length: { minimum: 3, maximum: 50 }, format: { with: /\A[a-zA-Z\s]+\z/, message: "Utilize somente letras" }
 end
