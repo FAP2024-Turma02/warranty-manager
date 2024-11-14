8ec65bdf6f8c6533022b9ed6aee5baac299d2d4e
MathiasSoares434 <mathiasernandes02@hotmail.com>
2024-11-14 11:17:34 -0300

    ajusta warranty controller

--- app/controllers/warranties_controller.rb
+++ app/controllers/warranties_controller.rb
@@ -1,33 +1,14 @@
 class WarrantiesController < ApplicationController
+
+  # Lista todas as warranties
   def index
-    @warranties = policy_scope(Warranty) # Usa o Pundit para filtrar os registros que o usuário pode acessar
-    render json: @warranties.map { |warranty| WarrantySerializer.call(warranty) }
+    @warranties = Warranty.all
+    render json: @warranties
   end
 
+  # Exibe uma warranty específica
   def show
-    authorize warranty
-    render json: WarrantySerializer.call(@warranty)
-  end
-
-  def update
-    authorize warranty
-    render json: @warranty.update(warranty_params) ? WarrantySerializer.call(@warranty) : { errors: @warranty.errors.full_messages },
-    status: @warranty.errors.any? ? :unprocessable_entity : :ok
-  end
-
-  def create
-    warranty = Warranty.new(warranty_params)
-    authorize @warranty # Garantir que o usuário tem permissão para criar uma warranty.
-
-    warranty.save ?
-      render(json: WarrantySerializer.call(@warranty), status: :created) :
-      render(json: { errors: @warranty.errors.full_messages }, status: :unprocessable_entity)
-  end
-
-  def destroy
-    authorize warranty 
-
-    warranty.destroy ? head(:no_content) : render(json: { error: 'Failed to delete warranty' }, status: :unprocessable_entity)
+    render json: warranty
   end
 
   private
@@ -36,9 +17,5 @@ class WarrantiesController < ApplicationController
     @warranty ||= Warranty.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'Warranty not found' }, status: :not_found
-  end
-
-  def warranty_params
-    params.require(:warranty).permit(:field1, :field2, :field3) # Substitua por os campos reais da sua warranty
   end
 end
