6747c0b0196578019093828ad5819b97c2d8b98d
MathiasSoares434 <mathiasernandes02@hotmail.com>
2024-11-14 10:35:21 -0300

    mudando a policy  da warranty novamente

--- app/policies/warranty_policy.rb
+++ app/policies/warranty_policy.rb
@@ -1,4 +1,32 @@
 class WarrantyPolicy < ApplicationPolicy
+  def index?
+    user.admin?
+  end
+
+  def show?
+    user.admin? || record.product_id == user.product_id
+  end
+
+  def create?
+    user.present?
+  end
+
+  def update?
+    if user.admin?
+      true
+    else
+      user.role == 'product' && record.product_id == user.product_id
+    end
+  end
+
+  def destroy?
+    user.admin?
+  end
+
+  def permitted_attributes
+    [:warranty_number, :issue_date, :expirity_date, :role, :admin, :validity_period]
+  end
+
   class Scope < ApplicationPolicy::Scope
     def resolve
       if user.admin?
@@ -8,32 +36,5 @@ class WarrantyPolicy < ApplicationPolicy
       end
     end
 
-    def index?
-      user.admin?
-    end
 
-    def show?
-      user.admin? || record.product_id == user.product_id
-    end
-
-    def create?
-      user.present?
-    end
-
-    def update?
-      if user.admin?
-        true
-      else
-        user.role == 'product' && record.product_id == user.product_id
-      end
-    end
-
-    def destroy?
-      user.admin?
-    end
-
-    def permitted_attributes
-      [:warranty_number, :issue_date, :expirity_date, :role, :admin, :validity_period]
-    end
-  end
 end
