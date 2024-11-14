654bd7d7c8177e242aad887d79781908752b0d32
Renan A <renanamendola@hotmail.com>
2024-11-13 23:06:51 -0300

    Rebase com a main

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,4 +32,8 @@ class ProductsController < ApplicationController
     params.require(:product).permit(:name, :price, :serial_number, :warranty_expiry_date, :invoices_id, :store_id)
   end
 
+    def product_params
+        params.require(:product).permit(:name, :price, :serial_number, :warranty_expiry_date, :invoice_id, :store_id)
+    end
+
 end
