dad323a781e554ea0f34e1f37bc3629ff6efd405
Felipe Rocha <rocha.felipe98@gmail.com>
2024-11-14 14:48:33 -0300

    Hotfix para confirmar os emails.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
 class User < ApplicationRecord
- include DeviseTokenAuth::Concerns::User
- has_many :invoices
- 
- devise :database_authenticatable, :registerable, :recoverable,
-        :rememberable, :validatable, :lockable
-  
- enum role: {user: 0, admin: 1}  
+  include DeviseTokenAuth::Concerns::User
+  has_many :invoices
 
- validates :name, presence: true, length: { minimum: 3, maximum: 50 }, format: { with: /\A[a-zA-Z\s]+\z/, message: "Utilize somente letras" } 
+  devise :database_authenticatable, :registerable, :recoverable,
+         :rememberable, :validatable, :confirmable, :lockable
+
+  before_create :skip_confirmation!
+  enum role: { user: 0, admin: 1 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 },
+                   format: { with: /\A[a-zA-Z\s]+\z/, message: 'Utilize somente letras' }
+
+  private
+
+  def skip_confirmation!
+    self.confirmed_at = Time.current
+  end
 end
