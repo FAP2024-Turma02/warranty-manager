f8c8c1f2980f4d52a4a838e04034e20551e6936c
Renan A <renanamendola@hotmail.com>
2024-11-14 01:02:38 -0300

    Criado e testado o crud de product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,4 @@
 class ProductsController < ApplicationController
-    
   def index
     @product = Product.all
     render json: @product
@@ -13,18 +12,21 @@ class ProductsController < ApplicationController
     @product = Product.create(product_params)
     render json: { status: 'success', message: 'Produto criado com sucesso', data: @product }, status: :created
   end
-  
+
   def update
-    authorize product
-    
     product.update!(product_params)
     render json: { status: 'success', message: 'Produto atualizado com sucesso', data: @product }, status: :ok
   end
 
+  def destroy
+    product.destroy
+    render json: { status: 'success', message: 'Produto deletado com sucesso'}, status: :ok
+    #head :no_content
+  end
 
   private
 
-  def product 
+  def product
     @product ||= Product.find(params[:id])
   end
 
