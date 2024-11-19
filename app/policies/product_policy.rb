4fd9a5dd16b332a3dc41e4a67dd48cbab0938ff5
Felipe Rocha <rocha.felipe98@gmail.com>
2024-11-19 09:42:26 -0300

    Update product_policy.rb
    Retirando comentários desnecessários

--- app/policies/product_policy.rb
+++ app/policies/product_policy.rb
@@ -1,9 +1,7 @@
 class ProductPolicy < ApplicationPolicy
-  # Define o escopo de acesso (usado em index)
   class Scope < Scope
     def resolve
       if user.admin?
-        # Admin pode acessar todos os produtos
         scope.all
       else
         # Usuário comum acessa produtos relacionados aos seus invoices
@@ -12,43 +10,36 @@ class ProductPolicy < ApplicationPolicy
     end
   end
 
-  # Permite visualizar um produto
   def show?
     user.admin? || user.invoices.exists?(id: record.invoice_id)
   end
 
-  # Permite listar os produtos
   def index?
     user.admin? || user_has_access_to_products?
   end
 
-  # Permite criar um produto
   def create?
     user.admin? || user.invoices.exists?(id: record.invoice_id)
   end
 
-  # Permite atualizar um produto
   def update?
     user.admin? || user.invoices.exists?(id: record.invoice_id)
   end
 
-  # Permite deletar um produto
   def destroy?
     user.admin? || user.invoices.exists?(id: record.invoice_id)
   end
 
-  # Permissões de atributos permitidos
   def permitted_attributes
     if record.new_record? # Verifica se é uma criação
       [:name, :description, :category, :price, :serial_number, :warranty_expiry_date, :store_id, :invoice_id]
-    else # Se não for criação, é atualização
+    else # Se não for criação, não passa o invoice_id
       [:name, :description, :category, :price, :serial_number, :warranty_expiry_date, :store_id]
     end
   end
 
   private
 
-  # Verifica se o usuário possui acesso a qualquer produto (via invoices)
   def user_has_access_to_products?
     user.invoices.joins(:products).exists?
   end
