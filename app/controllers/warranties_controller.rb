9bdace83d2de2bf96f0a02fe555336f50fa47592
Mathias Soares <mathiasernandes02@hotmail.com>
2024-11-23 15:52:36 -0300

    modificando o warranties_controller e o warranty_policy para que os mesmo possam assimilar tanto atributos por objeto quanto por classe

--- app/controllers/warranties_controller.rb
+++ app/controllers/warranties_controller.rb
@@ -1,36 +1,75 @@
 class WarrantiesController < ApplicationController
   def index
+
     @warranties = policy_scope(Warranty)
+
     render json: @warranties.map { |warranty| WarrantySerializer.call(warranty) }
+
   end
 
   def show
+
     authorize warranty
+
     render json: WarrantySerializer.call(warranty)
+
   end
 
-  def create
-    @warranty = Warranty.new(permitted_attributes(Warranty.new))
-    authorize @warranty 
+  def create       
+
+    @warranty = Warranty.new(permitted_attributes(Warranty))
+
+    authorize @warranty
+    
     @warranty.save!
+
     render json: WarrantySerializer.call(@warranty), status: :created
   end
 
   def update
+
     authorize warranty
+
     warranty.update!(permitted_attributes(warranty))
+
     render json: { status: 'success', message: 'Garantia atualizada com sucesso', data: warranty }, status: :ok
+
   end
 
   def destroy
+
     authorize warranty
+
     warranty.destroy
+
     render_deletion_message('Warranty')
+
   end
 
   private
 
-  def warranty
-    @warranty ||= Warranty.find(params[:id])
+  def permitted_attributes(klass)
+
+    params.require(:warranty).permit(:product_id, :warranty_number, :issue_date, :expirity_date, :validity_period)
+  
   end
+
+  def warranty
+
+    @warranty ||= Warranty.find(params[:id])
+
+  end
+
+  def render_created(resource)
+
+    render json: WarrantySerializer.call(resource), status: :created
+
+  end
+  
+  def render_errors(resource)
+
+    render json: { errors: resource.errors.full_messages }, status: :unprocessable_entity
+    
+  end
+
 end
--- app/policies/warranty_policy.rb
+++ app/policies/warranty_policy.rb
@@ -1,15 +1,17 @@
 class WarrantyPolicy < ApplicationPolicy
-    # Regra padrão para todos os métodos
+   
     def index?
-      user.admin? || user_has_access_to_product?
+      user.admin? || user_owns_warranty?
     end
   
     def show?
-      user.admin? || user_has_access_to_product?
+      user.admin? || user_owns_warranty?
     end
   
     def create?
-      user.admin? || user_has_access_to_product?
+      return user.admin? if record.is_a?(Class) 
+
+      user.admin? || user_has_access_to_product? 
     end
   
     def update?
@@ -29,26 +31,18 @@ class WarrantyPolicy < ApplicationPolicy
         end
       end
     end
-
-    def permitted_attributes
-      if record.new_record? # Verifica se é uma criação
-        [:warranty_number, :issue_date, :expirity_date, :validity_period, :product_id, :active]
-      else # Se não for criação, é atualização
-        [:warranty_number, :issue_date, :expirity_date, :validity_period, :active]
-      end
-    end
   
     private
   
-    def user_has_access_to_warranty?
-      record.product.invoice.user == user
-    end
   
     def user_owns_warranty?
       record.product.invoice.user == user
     end
   
     def user_has_access_to_product?
-      record.product.present? && record.product.invoice.user == user
+      product = record.product || Product.find_by(id: permitted_attributes(Warranty)[:product_id])
+      product&.invoice&.user == user
     end
-  end
+    
+
+end
